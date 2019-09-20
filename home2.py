f4e22aa14fa0a02f636b4cb57ec3c715a7df5f30
DariaKutkanych <55439869+DariaKutkanych@users.noreply.github.com>
2019-09-20 17:44:10 +0300

    added a new file with Homework2

---
+++ home2.py
@@ -0,0 +1,24 @@
+#task No 1
+name = input("Please enter your name: ").title()
+day = input("Please enter current day of the week: ").title()
+print("Good day %s! %s is a perfect day to learn some python." % (name, day))
+
+#task No 2
+first_name = "Daria"
+last_name = "Kutkanych"
+print("Hallo " + first_name + " " + last_name + "!")
+
+#task No 3
+a = int(input("Please input the number: "))
+b = int(input("Please input the number: "))
+
+print ("Addition ",a + b)
+print("Substraction ", a - b)
+print("Division ", a / b)
+print("Multiplikation ", a * b)
+print("Exponent ", a ** b)
+print("Modulus ", a % b)
+print("Floor division ", a // b)
+
+
+
